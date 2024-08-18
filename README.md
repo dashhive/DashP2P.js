e56715c45f88f54521b9869a8a7ee90fbe78864d
AJ ONeal <aj@therootcompany.com>
2024-08-17 22:32:55 -0600

    chore: create package & tooling config

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DashP2P.js
+
 Dash P2P Client for handling message streams via WebSocket or TCP
---
+++ dashp2p.js
@@ -0,0 +1,11 @@
+//@ts-ignore
+var DashP2P = ("object" === typeof module && exports) || {};
+(function (window, DashP2P) {
+  "use strict";
+
+  //@ts-ignore
+  window.DashP2P = DashP2P;
+})(globalThis.window || {}, DashP2P);
+if ("object" === typeof module) {
+  module.exports = DashP2P;
+}
